be21cb782103316d48a3bc4d611a1d1a1b3b6dcf
trejos <ivantrejos28@hotmail.com>
2017-03-27 17:34:55 -0500

    2017.1.0.1

--- install_lib.sh
+++ install_lib.sh
@@ -1,2 +1,26 @@
 #!/usr/bin/env bash
 
+sudo apt-get update
+sleep 4s
+
+sudo apt-get upgrade
+sleep 4s
+
+sudo apt-get install git-core
+sleep 4s
+
+sudo apt-get install git
+sleep 4s
+
+sudo apt-get install cmake
+sleep 4s
+
+sudo apt-get install libusb-1.0-0-dev
+sleep 4s
+
+sudo apt-get install build-essential
+sleep 4s
+
+
+
+
